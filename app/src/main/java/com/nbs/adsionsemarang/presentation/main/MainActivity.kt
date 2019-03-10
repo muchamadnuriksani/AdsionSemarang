c46f13f66ef83b2b955ee1a528d3da204dc3278d
sidiq permana <permana.sidiq@gmail.com>
2019-03-10 12:37:58 +0700

    Add koin support

--- app/src/main/java/com/nbs/adsionsemarang/presentation/main/MainActivity.kt
+++ app/src/main/java/com/nbs/adsionsemarang/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nbs.adsionsemarang.presentation.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import com.nbs.adsionsemarang.BuildConfig
@@ -11,19 +12,21 @@ import com.nbs.adsionsemarang.data.lib.ApiService
 import com.nbs.adsionsemarang.data.lib.OkHttpClientFactory
 import com.nbs.adsionsemarang.data.lib.getParameterInterceptor
 import com.nbs.adsionsemarang.presentation.main.model.Movie
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
-    private lateinit var presenter: MainPresenter
+    private val presenter: MainPresenter by inject{ parametersOf( this)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter = MainPresenter(this)
         presenter.getMovie()
     }
 
